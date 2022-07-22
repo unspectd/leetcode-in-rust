bed14bf49f2fe5c46213094a18226eb5b5a2ef84
unspectd <alexey.kovp@gmail.com>
2022-07-22 18:12:00 +0300

    15. 3Sum

--- src/problems/mod.rs
+++ src/problems/mod.rs
@@ -1,2 +1,3 @@
 mod p0001_two_sum;
 mod p0011_container_with_most_water;
+mod p0015_3sum;
---
+++ src/problems/p0015_3sum.rs
@@ -0,0 +1,66 @@
+pub struct Solution;
+
+use std::cmp::Ordering;
+
+impl Solution {
+    pub fn three_sum(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
+        nums.sort();
+        let mut result = Vec::new();
+
+        let mut i = 0;
+        while i < nums.len() - 2 && nums[i] <= 0 {
+            let mut lo = i + 1;
+            let mut hi = nums.len() - 1;
+
+            while lo < hi {
+                let sum = nums[i] + nums[lo] + nums[hi];
+                match sum.cmp(&0) {
+                    Ordering::Equal => {
+                        result.push(vec![nums[i], nums[lo], nums[hi]]);
+
+                        while lo < hi && nums[lo] == nums[lo + 1] {
+                            lo += 1;
+                        }
+                        lo += 1;
+                    }
+                    Ordering::Less => {
+                        lo += 1;
+                    }
+                    Ordering::Greater => {
+                        hi -= 1;
+                    }
+                }
+            }
+
+            while i < nums.len() - 2 && nums[i] == nums[i + 1] {
+                i += 1;
+            }
+            i += 1;
+        }
+
+        result
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        // (nums, result)
+        struct TestCase(Vec<i32>, Vec<Vec<i32>>);
+        let test_cases = [
+            TestCase(
+                vec![-1, 0, 1, 2, -1, -4],
+                vec![vec![-1, -1, 2], vec![-1, 0, 1]],
+            ),
+            TestCase(vec![0, 1, 1], vec![]),
+            TestCase(vec![0, 0, 0], vec![vec![0, 0, 0]]),
+        ];
+
+        for test_case in test_cases {
+            assert_eq!(Solution::three_sum(test_case.0), test_case.1);
+        }
+    }
+}
