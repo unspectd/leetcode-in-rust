985c7dfecbbc64da6de8820246fff2558346ddc2
unspectd <alexey.kovp@gmail.com>
2022-07-24 13:54:15 +0300

    55. Jump Game

---
+++ src/problems/p0055_jump_game.rs
@@ -0,0 +1,29 @@
+pub struct Solution;
+
+use std::cmp::max;
+
+impl Solution {
+    pub fn can_jump(nums: Vec<i32>) -> bool {
+        let mut max_reachable = 0;
+        for i in 0..nums.len() {
+            if i > max_reachable {
+                break;
+            }
+            max_reachable = max(max_reachable, i + nums[i] as usize);
+        }
+
+        max_reachable >= nums.len() - 1
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(Solution::can_jump(vec![0]), true);
+        assert_eq!(Solution::can_jump(vec![2, 3, 1, 1, 4]), true);
+        assert_eq!(Solution::can_jump(vec![3, 2, 1, 0, 4]), false);
+    }
+}
